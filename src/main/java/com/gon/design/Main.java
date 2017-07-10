1d57c88abf03e2e1e44ea0a749b94631f3a80307
Adelin_Ghanayem <Adelin_Ghanayem@epam.com>
2017-07-10 15:52:30 +0300

    master commit

---
+++ src/main/java/com/gon/design/Main.java
@@ -0,0 +1,12 @@
+package com.gon.design;
+
+/**
+ * Created by Adelin_Ghanayem on 10-Jul-17.
+ */
+public class Main {
+
+
+    public static void main(String[] string){
+        System.out.println("Hello GON ... ! ");
+    }
+}
