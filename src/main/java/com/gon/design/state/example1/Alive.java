4d5d6019969ffe26b3acc71e0d81dcdff459b7c8
Adelin_Ghanayem <Adelin_Ghanayem@epam.com>
2017-07-10 17:07:42 +0300

    implement state design pattern

---
+++ src/main/java/com/gon/design/state/example1/Alive.java
@@ -0,0 +1,43 @@
+package com.gon.design.state.example1;
+
+/**
+ * Created by Adelin_Ghanayem on 10-Jul-17.
+ */
+public class Alive implements PlayerState {
+
+
+    private Player player;
+
+    private int x;
+    private int y;
+    private int high;
+    private int bomb;
+
+    public void moveLeft(int x) {
+
+    }
+
+    public void moveRight(int x) {
+
+    }
+
+    public void jump(int height) {
+
+    }
+
+    public void standDown() {
+
+    }
+
+    public void standUp() {
+
+    }
+
+    public void fire() {
+
+    }
+
+    public void throwBomb() {
+
+    }
+}
