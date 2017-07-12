4dda177dffc1f8294923c5f4e461bbe330b25cc1
ZaraAndreeva <zara.andreeva@yahoo.com>
2017-07-12 15:55:20 +0300

    Add files via upload

---
+++ src/main/java/iterator/ChannelCollection.java
@@ -0,0 +1,13 @@
+package iterator;
+
+public interface ChannelCollection {
+
+	public void addChannel(Channel c);
+	
+	public void removeChannel(Channel c);
+	
+	public ChannelIterator iterator(ChannelTypeEnum type);
+	
+}
+
+
